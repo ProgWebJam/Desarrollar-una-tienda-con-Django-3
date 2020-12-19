95cde913d21c2a36f398d2b98193324e5d629f38
Jhon Marulanda <jhonmaruloo2012@gmail.com>
2020-12-19 17:17:26 -0500

    CURSO DE DJANGO: PROCESAR Y VALIDAR EL FORMULARIO DE REGISTRO + LOGIN

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import login
 
 # Create your views here.
 class VistaRegistro(View): 
@@ -9,4 +11,15 @@ class VistaRegistro(View):
         return render( request,'registro.html',{"form":form})
 
     def post(self, request):
-        return render( request,'registro.html')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            usuario = form.save()
+            nombre_usuario = form.cleaned_data.get("username")
+            #f = darle formato a una cadena
+            messages.success(request,f"Bienvenid@ a la plataforma {nombre_usuario}")
+            login(request, usuario)
+            return redirect("blog")
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+            return render( request,'registro.html',{"form":form})
