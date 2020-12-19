cfa630dbf43395ba8261a4968cdac46205f09954
Jhon Marulanda <jhonmaruloo2012@gmail.com>
2020-12-19 15:39:42 -0500

    CURSO DE DJANGO: CREAR BASE DE DATOS CON MIGRACIONES

---
+++ app/db.py
@@ -0,0 +1,9 @@
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'curso_django',
+        'USER': 'root',
+        'PASSWORD': 'j4mMySQL4dm*',
+        'PORT': '3306',
+    }
+}
