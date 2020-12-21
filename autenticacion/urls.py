688fdc7aa71d2336bf79b5b60dc33e11cf8b5d3d
Jhon Marulanda <jhonmaruloo2012@gmail.com>
2020-12-21 10:19:46 -0500

    CURSO DE DJANGO: PROCESO COMPLETO PARA INICIAR SESIÓN

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 #from .views import registro
-from .views import VistaRegistro, salir
+from .views import VistaRegistro, salir, acceder
 
 urlpatterns = [
     path('registro/', VistaRegistro.as_view(), name="registro"),
+    path('acceder/', acceder, name="acceder"),
     path('salir/',salir, name="salir"),
 ]
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 #Registrar Usuario
+
+def acceder(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            nombre_usuario = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            usuario = authenticate(username=nombre_usuario,password=password)
+
+            if usuario is not None:
+                login(request,usuario)
+                messages.success(request,f"Bienvenid@ de nuevo a la plataforma {nombre_usuario}")
+                return redirect("blog")
+            else:
+                messages.error(request, "Los datos son incorrectos")
+        else:
+            messages.error(request, "Los datos son incorrectos")        
+
+    form = AuthenticationForm
+    return render(request,'acceder.html',{"form":form})
+
 class VistaRegistro(View): 
     def get(self, request):
         form = UserCreationForm()
@@ -12,6 +34,7 @@ class VistaRegistro(View):
 
     def post(self, request):
         form = UserCreationForm(request.POST)
+
         if form.is_valid():
             usuario = form.save()
             nombre_usuario = form.cleaned_data.get("username")
@@ -19,6 +42,7 @@ class VistaRegistro(View):
             messages.success(request,f"Bienvenid@ a la plataforma {nombre_usuario}")
             login(request, usuario)
             return redirect("blog")
+
         else:
             for msg in form.error_messages:
                 messages.error(request, form.error_messages[msg])
@@ -28,4 +52,4 @@ class VistaRegistro(View):
 def salir(request):
     logout(request)
     messages.success(request,f"Tu sesion se ha cerrado correctamente")
-    return redirect("blog")
+    return redirect("acceder")
