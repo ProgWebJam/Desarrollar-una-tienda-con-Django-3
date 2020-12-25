a36e8df3e9373a82c85dcdb118126e024f948bc3
Jhon Marulanda <jhonmaruloo2012@gmail.com>
2020-12-25 13:49:49 -0500

    CURSO DE DJANGO: PROTEGER URLS QUE NECESITEN AUTENTICACIÓN CON DECORADORES

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,9 @@ from blog.forms import FormularioPost
 from django.contrib import messages
 from blog.models import Post
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/accounts/acceder')
 def index(request):
     listado_posts = Post.objects.all()
     #se va a paginar de 3
@@ -19,6 +21,7 @@ def index(request):
     paginas = range(1, posts.paginator.num_pages +1)
     return render(request,"blog.html",{"posts":posts, "paginas": paginas, "pagina_actual": pagina_actual})
 
+@login_required(login_url='/accounts/acceder')
 def crear_post(request):
     if request.method == "POST":
         form = FormularioPost(request.POST, request.FILES)
@@ -37,6 +40,7 @@ def crear_post(request):
     form = FormularioPost()
     return render(request,"crear_post.html",{"form":form})
 
+@login_required(login_url='/accounts/acceder')
 def eliminar_post(request,post_id):
     try:
         post = Post.objects.get(pk=post_id)
