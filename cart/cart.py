41af086ebd27c7698aa52cc2489c7c133010d119
Jhon Marulanda <jhonmaruloo2012@gmail.com>
2021-01-01 13:21:09 -0500

    TIENDA CON DJANGO 3: CREAR APLICACIÓN CARRITO Y CARRITO DE COMPRAS

---
+++ cart/cart.py
@@ -0,0 +1,57 @@
+class Cart:
+    #Iniciamos carrito de compra
+    def __init__(self,request):
+        self.request = request
+        self.session = request.session
+        cart = self.session.get("cart")
+        if not cart:
+            cart = self.session["cart"] = {}
+        self.cart = cart
+        
+    #agregamos producto
+    def add (self,product):
+        if str(product.id) not in self.cart.keys():
+            self.cart[product.id] = {
+                "product_id": product.id,
+                "name": product.name,
+                "quantity": 1,
+                "price": str(product.price),
+                "image": product.image.url
+            }
+        else:
+            for key, value in self.cart.items():
+                if key == str(product.id):
+                    value["quantity"] = value["quantity"] + 1
+                    break
+        self.save()            
+
+    #guardamos
+    def save(self):
+        self.session["cart"] = self.cart
+        #guardamos sesion
+        self.session.modified = True
+
+    #Eliminar un producto
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    #decrementamos producto
+    def decrement(self, product):
+        for key, value in self.cart.items():
+            if key == str(product.id):
+                value["quantity"] = value["quantity"] - 1
+                if value["quantity"] < 1:
+                    self.remove(product)
+                else:
+                    self.save()
+                break
+            else:
+                print("El producto no existe en el carrito")
+
+    #Limpiar carrito
+    def clear(self):
+        self.session["cart"] = {}
+        self.session.modified = True
