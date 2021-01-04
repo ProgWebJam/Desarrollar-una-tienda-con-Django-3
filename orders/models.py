c0eb61bc02dff6017d7d4387730ec105a6035d5e
Jhon Marulanda <jhonmaruloo2012@gmail.com>
2021-01-04 16:46:56 -0500

    TIENDA CON DJANGO 3: CREAR APLICACIÓN DE PEDIDOS Y DEFINIR MODELOS ORDER Y ORDERLINE

---
+++ orders/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models import F, Sum, FloatField
+from products.models import Product
+
+#Modelo Usuario
+User = get_user_model()
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    completd = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def total(self):
+        return self.OrderLine.set.aggregate(
+            #F = saca toda la informacion que necesitamos
+            total = sum( F("product__price")*F("quantity"), output_field=FloatField())
+        )["total"] or FloatField(0)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        db_table = 'orders'
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
+        ordering = ['id']
+
+class OrderLine(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.quantity} de {self.product.name}'
+
+    class Meta:
+        db_table = 'orderlines'
+        verbose_name = 'Linea de pedido'
+        verbose_name_plural = 'Linea de pedidos'
+        ordering = ['id']
