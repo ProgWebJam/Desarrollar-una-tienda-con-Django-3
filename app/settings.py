422607cf4224cbd98033bcff1aac27b0238e1518
Jhon Marulanda <jhonmaruloo2012@gmail.com>
2021-01-11 15:09:12 -0500

    Configuracion sqlite3

--- app/settings.py
+++ app/settings.py
@@ -81,7 +81,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = MYSQL
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
 
 
 # Password validation
